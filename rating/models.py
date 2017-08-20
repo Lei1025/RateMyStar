565ede910d991c572067aad7ea66938dffa198d2
Lei1025 <llei1025@gmail.com>
2017-08-20 15:00:13 -0400

    detail page

--- rating/models.py
+++ rating/models.py
@@ -1,10 +1,11 @@
 from django.conf import settings
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User,UserManager
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from PIL import Image
 import datetime
 from decimal import Decimal
+
 # Create your models here.
 class IMG(models.Model):
     img = models.ImageField(upload_to='rating/media/upload')
@@ -13,18 +14,20 @@ class IMG(models.Model):
         return self.id
 
 class R_User(models.Model):
-    username=models.ForeignKey(settings.AUTH_USER_MODEL)
-    profile_picture=models.ImageField(upload_to='rating/media/profile')
+    user=models.OneToOneField(User,default='new user')
+    profile=models.ImageField(upload_to='profile',default='profile/user.png')
+
     def __str__(self):
-        return self.username
+        return str(self.user)
 
 class Article(models.Model):
     CATEGORY_CHOICES=(
         ('Musicians','Musicians'),
         ('Authors','Authors'),
         ('Actors','Actors'),
-        ('Athletes','Athlets'),
-        ('Hosts','Hosts')
+        ('Athletes','Athletes'),
+        ('Models', 'Models'),
+        ('Hosts','Hosts'),
     )
     #id = models.AutoField(primary_key=True)
     author=models.ForeignKey(settings.AUTH_USER_MODEL,default='Username')
@@ -33,15 +36,15 @@ class Article(models.Model):
     age=models.PositiveIntegerField(default=0)
     category=models.CharField(choices=CATEGORY_CHOICES,max_length=100)
     nationality=models.CharField(max_length=100)
-    thumbnail=models.ImageField(upload_to='rating/media/article/thumbnail',default="Please uplaod a thumbnail picture.")
-    banner=models.ImageField(upload_to='rating/media/article/banner',default="Please upload a banner picture.")
+    thumbnail=models.ImageField(upload_to='article/thumbnail',default="Please uplaod a thumbnail picture.")
+    banner=models.ImageField(upload_to='article/banner',default="Please upload a banner picture.")
     content=models.TextField(max_length=10000)
 
     def __str__(self):
         return self.title
 
 class Score(models.Model):
-    user=models.ForeignKey(settings.AUTH_USER_MODEL,default='username')
+    user = models.OneToOneField(User, default='new user')
     title=models.ForeignKey(Article)
     C1=models.DecimalField(decimal_places=1,max_digits=2,default=Decimal(0.0))
     C2=models.DecimalField(decimal_places=1,max_digits=2,default=Decimal(0.0))
@@ -50,3 +53,12 @@ class Score(models.Model):
     C5 = models.DecimalField(decimal_places=1, max_digits=2, default=Decimal(0.0))
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    user = models.ForeignKey(R_User)
+    article=models.ForeignKey(Article)
+    time=models.DateTimeField()
+    text=models.TextField(max_length=500)
+    def __str__(self):
+        return str(self.user) +' posted in '+str(self.article)
+
--- rating/views.py
+++ rating/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
-
 from rating.models import *
+
 # Create your views here.
 def uploadImg(request):
     if request.method == 'POST':
@@ -28,10 +28,12 @@ def register(request):
         firstname=request.POST['Firstname']
         lastname = request.POST['Lastname']
         email = request.POST['Email']
+        username=firstname+'.'+lastname
         if password!=password2:
             return render(request,'register.html',{'error':'Passwords do not match!'})
         elif password==password2:
-            User.objects.create_user(username=firstname+'_'+lastname,password=password,first_name=firstname,last_name=lastname,email=email)
+            User.objects.create_user(username=username,password=password,first_name=firstname,last_name=lastname,email=email)
+            R_User.objects.create(user=User.objects.get(username=username))
             return render(request,'index.html')
     else:
         return render(request,'register.html')
@@ -68,7 +70,19 @@ def index_content(request):
     return render(request,'Puzzles/index-content.html')
 
 def detail(request):
-    return render(request, 'detail.html')
+    if request.method=='POST':
+        username=request.user
+        time=datetime.datetime.now()
+        comment=request.POST['comment']
+        Comment.objects.create(user=R_User.objects.get(user__username=username),article=Article.objects.get(title='Katy Perry'),time=time,text=comment)
+
+    count=Comment.objects.filter(article__title='Katy Perry').count()
+    comment=Comment.objects.filter(article__title='Katy Perry')
+    try:
+        article=Article.objects.get(title='Katy Perry')
+    except:
+        return render(request, 'detail.html',{'error':'No Data'})
+    return render(request, 'detail.html',{'article':article,'count':count,'comment':comment})
 
 def category(request):
     direction=['right','right','left','left']
